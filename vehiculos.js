4e99223d85dbb63ae8e6bb54d6202a5047731749
MaraSatilu <marasatilu@gmail.com>
2022-12-08 21:38:07 +0100

    He creado una pagina html junto con el css y he definido los objetos coche, moto, patinete t bicicleta.

---
+++ vehiculos.js
@@ -0,0 +1,28 @@
+const coche = {
+	numeroDePuertas: [3 || 5],
+	velocidadMaxima:_.range(0, 100),
+	numeroDeRuedas: 4,
+    esMotorizado: true,
+};
+
+const moto = {
+	numeroDePuertas: 0,
+	velocidadMaxima: _.range(0, 80),
+	numeroDeRuedas: 2,
+    esMotorizado: true,
+};
+
+const patineteElectrico = {
+	numeroDePuertas: 0,
+	velocidadMaxima: _.range(0, 40),
+	numeroDeRuedas: 2,
+    esMotorizado: true,
+};
+
+const bicicleta = {
+	numeroDePuertas: 0,
+	velocidadMaxima: _.range(0, 50),
+	numeroDeRuedas: 2,
+    esMotorizado: false,
+};
+
