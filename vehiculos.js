1f4f6fbab1115f84354bc15c92f9bfeb647dc1f8
MaraSatilu <marasatilu@gmail.com>
2022-12-11 20:41:51 +0100

    añado losnombres en ingles y creo funcion y array

--- vehiculos.js
+++ vehiculos.js
@@ -1,33 +1,47 @@
-const coche = {
-	numeroDePuertas: [3 || 5],
-	velocidadMaxima:_.range(0, 100),
-	numeroDeRuedas: 4,
-    esMotorizado: true,
+const vehicle1 = {
+	doors: [3 || 5],
+	velMax:range=(0, 100),
+	numWheels: 4,
+    otor: true,
 };
 
-const moto = {
-	numeroDePuertas: 0,
-	velocidadMaxima: _.range(0, 80),
-	numeroDeRuedas: 2,
-    esMotorizado: true,
+const vehicle2 = {
+	doors: 0,
+	velMax: range=(0, 80),
+	numWheels: 2,
+    motor: true,
 };
 
-const patineteElectrico = {
-	numeroDePuertas: 0,
-	velocidadMaxima: _.range(0, 40),
-	numeroDeRuedas: 2,
-    esMotorizado: true,
+const vehicle3 = {
+	doors: 0,
+	velMax: range=(0, 40),
+	numWheels: 2,
+    motor: true,
 };
 
-const bicicleta = {
-	numeroDePuertas: 0,
-	velocidadMaxima: _.range(0, 50),
-	numeroDeRuedas: 2,
-    esMotorizado: false,
+const vehicle4 = {
+	doors: 0,
+	velMax: range=(0, 50),
+	numWheels: 2,
+    motor: false,
 };
 
 
-const vehiculoDesconocido ={
 
+
+function vehicle (){
+	if(vehicle1){
+		return 'Coche';
+	}else if(vehicle2){
+		return 'Moto';
+	}else if(vehicle3) {
+		return 'Patinete eléctrico';
+	}else if(vehicle4){
+		return '`Bicicleta';
+	}
+	return 'Vehiculo desconocido';
+	
 }
 
+const vehicles=[vehicle1, vehicle2, vehicle3, vehicle4];
+console.log(vehicles);
